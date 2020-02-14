63251a66b8d53eb754994a164c8fd138ba888303
Claire Bullock <bullockcm13@gmail.com>
2020-02-14 15:54:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array = ["one", "two"]
-  new_array.push("three")
+  
 end
 
 def add_element_to_start_of_array(array, element)
